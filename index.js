b936c83cb82e49ac0ac191fad0d4e64598717886
Christian Terence <chrisc98@vt.edu>
2019-01-04 14:13:26 -0500

    Fixes on Index.js

--- index.js
+++ index.js
@@ -30,13 +30,13 @@ require("./routes/spotify")(app);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+  app.use(express.static('front/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
   });
 }
 
